845efa6734dfcf611b033cbdf7d1742b850f929d
marwankous <o0xmarionx0o@gmail.com>
2023-03-27 23:50:49 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,34 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
 
-#define PASSWORD_LENGTH 15
-#define MAX_ATTEMPTS 1000
-
-void generate_password(char *password) {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i, random_index;
-    srand(time(NULL));
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        random_index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[random_index];
-    }
-    password[PASSWORD_LENGTH] = '\0';
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 }
-
-int main() {
-    char password[PASSWORD_LENGTH + 1];
-    int attempts = 0;
-    do {
-        generate_password(password);
-        attempts++;
-    } while (strcmp(password, "Tada! Congrats") != 0 && attempts < MAX_ATTEMPTS);
-    if (strcmp(password, "Tada! Congrats") == 0) {
-        printf("%s\n", password);
-    } else {
-        printf("Unable to generate password within %d attempts.\n", MAX_ATTEMPTS);
-    }
-    return 0;
-}
-
