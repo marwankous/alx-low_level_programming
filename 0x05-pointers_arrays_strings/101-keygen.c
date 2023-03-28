7607f79d8a075b6ab193e4fc3228bd223fdff101
marwankous <o0xmarionx0o@gmail.com>
2023-03-27 22:26:50 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,53 +1,20 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void randomPasswordGeneration() {
-  const char numbers[] = "0123456789";
-  const char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
-  const char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const char symbols[] = "!@#$%^&*()_-+={}[]\\|:;\"'<>,.?/";
-
-  const int N = 10;
-  char password[N + 1];
-  int i, rand_num;
-
-  srand(time(NULL));
-
-  for (i = 0; i < N; i++) {
-    rand_num = rand() % 4;
-    switch (rand_num) {
-      case 0:
-        password[i] = numbers[rand() % strlen(numbers)];
-        break;
-      case 1:
-        password[i] = lowercase[rand() % strlen(lowercase)];
-        break;
-      case 2:
-        password[i] = uppercase[rand() % strlen(uppercase)];
-        break;
-      case 3:
-        password[i] = symbols[rand() % strlen(symbols)];
-        break;
+void generate_password() {
+    const char* charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const int len = 12;
+    char password[len+1];
+    srand(time(NULL));
+    for (int i = 0; i < len; i++) {
+        password[i] = charset[rand() % 62];
     }
-  }
-
-  password[N] = '\0';
-  printf("%s", password);
+    password[len] = '\0';
+    printf("%s\n", password);
 }
 
-/**
- * main - generates a valid password for 101-crackme
- *
- * Return: Always 0
- */
-
-int main()
-{
-	int N = 10;
-
-	randomPasswordGeneration(N);
-
-	return 0;
+int main() {
+    generate_password();
+    return 0;
 }
