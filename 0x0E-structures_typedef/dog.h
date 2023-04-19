0d0fbaea4da37a7c03ecd1fc7cbfbb0f1a37430d
m.kouskous <m.kouskous@graviton.ma>
2023-04-19 02:46:00 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Information about dog
+ * @name: Dog's name
+ * @owner: Dog's owner name
+ * @age: Age of dog
+ *
+ * Description: this struct contains information about the dog, its age and its owner name
+ */
+struct dog
+{
+    char *name;
+    char *owner;
+    float age;
+};
