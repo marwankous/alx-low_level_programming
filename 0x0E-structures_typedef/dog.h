bf764ca14c57b09bf957f0218dc28a73b46d728a
m.kouskous <m.kouskous@graviton.ma>
2023-04-19 03:02:06 +0000

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - Information about dog
- * @name: Dog's name
- * @owner: Dog's owner name
- * @age: Age of dog
+ * struct dog - dog structure
+ * code by ArchibaldTK
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
  *
- * Description: this struct contains information about the dog, its age and its owner name
  */
 struct dog
 {
     char *name;
-    char *owner;
     float age;
+    char *owner;
 };
 
 /**
  * dog_t -Typedef for struct dog
  */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* struct dog */
