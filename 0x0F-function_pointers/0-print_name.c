7ee0a130e3216446200979cf5ed60c8db60a6b79
marwankous <marouane.kouskous@gmail.com>
2023-04-20 18:58:47 +0000

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - Prints a name
+ * @name: A pointer to a string containing a name
+ * @f: A pointer to a function that takes a
+ * char * as an argument and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
